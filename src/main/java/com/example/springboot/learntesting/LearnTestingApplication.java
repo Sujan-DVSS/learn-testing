ffaaac56c85a52406128f3f30324e948b4746a69
Sujan-DVSS <63912098+Sujan-DVSS@users.noreply.github.com>
2023-11-18 21:26:19 +0530

    Project Structure Creation

---
+++ src/main/java/com/example/springboot/learntesting/LearnTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.learntesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot/learntesting/LearnTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.learntesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
